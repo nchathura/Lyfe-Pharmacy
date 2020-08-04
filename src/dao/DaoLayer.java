7a9cb6c6e59e479ccac57096695f5f16bf3ce6e3
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-04 23:46:39 +0530

    Added Order Crud Operations In The DAO Layer

--- src/dao/DaoLayer.java
+++ src/dao/DaoLayer.java
@@ -1,9 +1,7 @@
 package dao;
 
 import db.DBConnection;
-import util.AgentTM;
-import util.CompanyTM;
-import util.EmployeeTM;
+import util.*;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -339,6 +337,112 @@ public class DaoLayer {
         return false;
 
     }
+
+    //===================================================================================================================================
+
+    public static List<Order> getAllOrders(){
+        ArrayList<Order> orders = new ArrayList<>();
+        try {
+
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM `order`");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                orders.add(new Order(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getDate(3).toLocalDate()
+
+
+                ));
+
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return orders;
+
+    }
+    public static Order getOrder(String companyId) {
+        try {
+
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT *  FROM `order` WHERE orderId=(?)");
+            preparedStatement.setObject(1, companyId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new Order(
+                        resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getDate(3).toLocalDate()
+
+                );
+
+
+            }
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static boolean saveOrder(Order order){
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO `order` VALUES (?,?,?)");
+            preparedStatement.setObject(1, order.getOrderId());
+            preparedStatement.setObject(2, order.getEmpId());
+            preparedStatement.setObject(3, order.getOrderDate());
+
+
+
+
+
+            return preparedStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+
+
+    }
+    public static boolean deleteOrder(String orderId){
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM `order`  WHERE orderId=(?)");
+            preparedStatement.setObject(1, orderId);
+            return preparedStatement.executeUpdate()>0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+
+
+    }
+    public static boolean updateOrder(Order order){
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE `order` SET empId=(?),orderDate=(?) WHERE orderId=(?)");
+
+            preparedStatement.setObject(1, order.getEmpId());
+            preparedStatement.setObject(2, order.getOrderDate());
+            preparedStatement.setObject(3, order.getOrderId());
+
+
+
+
+            return preparedStatement.executeUpdate()>0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
 
 
 
