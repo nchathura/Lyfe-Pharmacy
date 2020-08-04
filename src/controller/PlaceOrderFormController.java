bf6924d3ef15c2ac291016da3c60ae72a26a6a3c
Nchathura <chathuranganadeepa@gmail.com>
2020-08-04 22:02:25 +0530

    created app initializer, added new asset, add controllers

---
+++ src/controller/PlaceOrderFormController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class PlaceOrderFormController {
+    public AnchorPane root;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlaceOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void navigate(MouseEvent mouseEvent) throws IOException {
+        URL resource = this.getClass().getResource("/view/MainForm.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        Stage primaryStage = (Stage) (this.root.getScene().getWindow());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+    }
+}
