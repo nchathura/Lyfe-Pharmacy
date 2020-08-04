4100dcaeb6614cb4d764d1456eb3f08a7c4525a9
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-04 19:49:21 +0530

    Added dao layer and basic methods

---
+++ src/dao/DaoLayer.java
@@ -0,0 +1,28 @@
+package dao;
+
+import util.EmployeeTM;
+
+import java.util.List;
+
+public class DaoLayer {
+    public static List<EmployeeTM> getAllEmployees(){
+
+
+
+
+    }
+    public static EmployeeTM getEmployee(String employeeId){
+
+
+    }
+    public static boolean saveEmployee(EmployeeTM employeeTM){
+
+    }
+    public static boolean deleteEmployee(String employeeId){
+
+    }
+    public static boolean updateEmployee(EmployeeTM employeeTM){
+
+
+    }
+}
