5a64c070d9ecfd11491cce646c243e809d00b7a9
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-05 23:17:16 +0530

    Added OrderDetail Entity Content

--- src/entity/OrderDetail.java
+++ src/entity/OrderDetail.java
@@ -1,4 +1,62 @@
 package entity;
 
+import java.math.BigDecimal;
+
 public class OrderDetail {
+    private String orderId;
+    private String itemCode;
+    private BigDecimal qty;
+    private BigDecimal unitPrice;
+
+    @Override
+    public String toString() {
+        return "OrderDetail{" +
+                "orderId='" + orderId + '\'' +
+                ", itemCode='" + itemCode + '\'' +
+                ", qty=" + qty +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(String orderId, String itemCode, BigDecimal qty, BigDecimal unitPrice) {
+        this.orderId = orderId;
+        this.itemCode = itemCode;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public BigDecimal getQty() {
+        return qty;
+    }
+
+    public void setQty(BigDecimal qty) {
+        this.qty = qty;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 }
