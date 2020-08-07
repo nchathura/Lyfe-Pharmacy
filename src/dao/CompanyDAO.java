fe681a95b5d7ffbe1f4dc40866d9face73a3bfae
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-07 19:20:28 +0530

    Added CompanyDAO interface Contents

--- src/dao/CompanyDAO.java
+++ src/dao/CompanyDAO.java
@@ -1,4 +1,13 @@
 package dao;
 
+import entity.Company;
+
+import java.util.List;
+
 public interface CompanyDAO {
+    public List<Company> getAllCompanies();
+    public  Company getCompany(String companyId);
+    public  boolean saveCompany(Company company);
+    public  boolean deleteCompany(String companyId);
+    public  boolean updateCompany(Company company);
 }
