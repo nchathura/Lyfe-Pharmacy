39976d8b88af88de64439dd74659f565a8f1b94c
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-07 19:19:26 +0530

    Added AgentDAO interface content

--- src/dao/AgentDAO.java
+++ src/dao/AgentDAO.java
@@ -1,4 +1,23 @@
 package dao;
 
+import db.DBConnection;
+import entity.Agent;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public interface AgentDAO {
+    public List<Agent> getAllAgents();
+    public  Agent getAgent(String agentId);
+    public  boolean saveAgent(Agent agent);
+    public  boolean deleteAgent(String agentId);
+    public  boolean updateAgent(Agent agent);
+
+
+
+
 }
