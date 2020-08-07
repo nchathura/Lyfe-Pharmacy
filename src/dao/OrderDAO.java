3fcf8594a2ca785a81ff15bf322d910b77029f88
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-07 19:26:29 +0530

    Added OrderDAO interface content

--- src/dao/OrderDAO.java
+++ src/dao/OrderDAO.java
@@ -1,4 +1,19 @@
 package dao;
 
+import entity.Order;
+
+import java.util.List;
+
 public interface OrderDAO {
+    public List<Order> getAllOrders();
+    public  Order getOrder(String orderId);
+    public  boolean saveOrder(Order order);
+    public  boolean deleteOrder(String orderId);
+    public  boolean updateOrder(Order order);
+
+
+
+
+
+
 }
