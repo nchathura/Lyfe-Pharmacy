259d56fb4d9154ff84e9aacac0849261f6d8822d
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-07 19:21:34 +0530

    Added CompanyItemDAO interface content

--- src/dao/CompanyItemDAO.java
+++ src/dao/CompanyItemDAO.java
@@ -1,4 +1,16 @@
 package dao;
 
+import entity.CompanyItem;
+import entity.CompanyItemPK;
+
+import java.util.List;
+
 public interface CompanyItemDAO {
+    public List<CompanyItem> getAllCompanyItems();
+    public  CompanyItem getCompanyItem(CompanyItemPK companyItemPK);
+    public  boolean saveCompanyItem(CompanyItem companyItem);
+    public  boolean deleteCompanyItem(CompanyItemPK companyItemPK);
+    public  boolean updateCompanyItem(CompanyItem companyItem)
+
+
 }
