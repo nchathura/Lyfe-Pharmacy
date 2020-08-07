c7be088480b2e53a18727191b596d37226ebd2ce
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-07 19:25:26 +0530

    Added itemDAO interface content

--- src/dao/ItemDAO.java
+++ src/dao/ItemDAO.java
@@ -1,4 +1,16 @@
 package dao;
 
+import entity.Item;
+
+import java.util.List;
+
 public interface ItemDAO {
+    public List<Item> getAllItems();
+    public  Item getItem(String itemCode);
+    public  boolean saveItem(Item item);
+    public  boolean deleteItem(String itemId);
+    public  boolean updateItem(Item item);
+
+
+
 }
