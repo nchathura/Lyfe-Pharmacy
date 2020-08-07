3b4f378df7d12dbee73029ad187a2be3f1144362
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-07 19:24:55 +0530

    Added itemDAO interface content

--- src/dao/EmployeeDAO.java
+++ src/dao/EmployeeDAO.java
@@ -1,4 +1,14 @@
 package dao;
 
+import entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+    public  Employee getEmployee(String empId);
+    public  boolean saveEmployee(Employee employee);
+    public  boolean deleteEmployee(String employeeId);
+    public  boolean updateEmployee(Employee employee);
+
 }
